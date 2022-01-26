005527c1ed30b2b32d63b15c9cb06880bfa90ad8
mikitaro-27 <matt.diak@gmail.com>
2022-01-26 14:20:31 +0000

    step 17

--- generatesMessage.js
+++ generatesMessage.js
@@ -1,33 +1,19 @@
-const createMessage = (name) => {
-  return `Hi ${name}! 50% off our best candies for you today!`
-}
-// Write a function generateMessages 
-const generateMessages = (arrayNames) => { 
-  return arrayNames.map(createMessage)
-}
-// that takes an array of names and 
-// returns an array of personalised messages for each name. 
+const createMessage = (object) => {
+  return `Hi ${object.name}! ${object.discount}% off our best candies for you today!`
+};
 
-// For example, when given the array above, 
-// the function should return an array with the first element 
-// being the string 'Hi Anna! 50% off our best candies for you today!', 
-// the second element the string 'Hi Laura! 50% off our best candies for you today!', 
-// and so on.
+const generateMessages = (arrayNames) => {
+  return arrayNames.map(createMessage);
+};
 
-//setting numbers = an array of numbers
-let numbers = [1, 4, 9]
-//setting doubles = map iterating the function with parameter num over each of the numbers in the arrayNames
-let doubles = numbers.map(function(num) {
-  return num * 2 //return each number * 2
-})
+// objectNames
 
+const namesAndDiscounts = [
+  { name: 'Anna', discount: 50 },
+  { name: 'Laura', discount: 40 },
+  { name: 'Josh', discount: 30 },
+  { name: 'Min', discount: 50 },
+  { name: 'Karla',discount:  6 }
+];
 
-// replacing the function createMessage within the generateMessages
-// const createMessage = (name) => {
-//   return `Hi ${name}! 50% off our best candies for you today!`
-// }
-// // Write a function generateMessages 
-// const generateMessages = (arrayNames) => { 
-//   arrayNames.map(name => `Hi ${name}! 50% off our best candies for you today!`)
-
-// }
+console.log(generateMessages(namesAndDiscounts))
